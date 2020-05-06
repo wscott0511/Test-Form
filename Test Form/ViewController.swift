a56d4e576d014c197159ee0c76b4d81442fbb675
Ryan Scott <williamscott0511@gmail.com>
2020-05-06 10:34:08 -0700

    First test
    initial commit

---
+++ Test Form/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Test Form
+//
+//  Created by William Scott on 5/1/20.
+//  Copyright © 2020 William Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet var typePicker: [UIPickerView]!
+    
+}
+
